2f15fe4331c55e2e8d17fd22cc395a05e4cafed4
huangboju <bhuang@xiaohongshu.com>
2019-07-15 19:28:39 +0800

    Update Package.swift

--- WebApp/Package.swift
+++ WebApp/Package.swift
@@ -7,8 +7,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/template-kit.git", from: "1.4.0")
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0")
     ],
     targets: [
         .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Authentication", "Leaf"]),
