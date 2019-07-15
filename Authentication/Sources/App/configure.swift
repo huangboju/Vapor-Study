2974285cb398d566ffdcf5cbd1cb02d7b877e1b2
huangboju <bhuang@xiaohongshu.com>
2019-07-16 07:57:48 +0800

    refactor pod

--- Authentication/Sources/App/configure.swift
+++ Authentication/Sources/App/configure.swift
@@ -17,15 +17,15 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
-
+    
     // Configure a SQLite database
     let sqlite = try SQLiteDatabase(storage: .memory)
-
+    
     /// Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
     databases.add(database: sqlite, as: .sqlite)
     services.register(databases)
-
+    
     /// Configure migrations
     var migrations = MigrationConfig()
     migrations.add(model: User.self, database: .sqlite)
