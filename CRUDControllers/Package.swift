163df886e3ae6e9ebc8f9c12793cde06c780e2ff
huangboju <bhuang@xiaohongshu.com>
2019-07-16 08:12:49 +0800

    修改

--- CRUDControllers/Package.swift
+++ CRUDControllers/Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "CRUDControllers",
+    products: [
+        .library(name: "CRUDControllers", targets: ["App"]),
+    ],
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
@@ -12,6 +15,8 @@ let package = Package(
     ],
     targets: [
         .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
-        .target(name: "Run", dependencies: ["App"])
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"])
     ]
 )
+
